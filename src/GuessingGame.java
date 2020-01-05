0129a498d118d2fdb762436d9f773e7ce5f180da
KariNoir <KariNoirm@gmail.com>
2020-01-05 13:04:12 -0500

    Almost to the full solution

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -1,50 +1,56 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class GuessingGame {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
 		Scanner number = new Scanner(System.in);
 		System.out.println("Guess a number");
-		
+
 		int numberG = number.nextInt();
+
+		int guessingAttempts = 1;
 		
-		int guessingAttempts = 0;
+		Random random = new Random();
 		
-		while(guessingAttempts <3) {
-			//System.out.println(";three tries");
-			guessingAttempts +=1;
+		int randomNumber = random.nextInt(4-0); //the random numbers range. change back to 11-0 and finish.
 		
-		if(numberG == -1) {
-			System.out.println("you lost");
-			System.exit(numberG);
+
+		while (guessingAttempts < 3) {
+			System.out.println(";You get three tries");
+			if (guessingAttempts == -1) {
+			System.out.println("You lose!!!");
 			}
-		
+
+			// while(guessingAttempts !=7 || guessingAttempts !=0) {
+			// System.out.println(";three tries");
+			// guessingAttempts = number.nextInt();
+
+			if (numberG == randomNumber) {
+				System.out.println("You are a winner");
+				break;
+			} else if (numberG == -1) {
+				System.exit(numberG);
 				
-		else if(numberG == 7) {
-			System.out.println("You are a winner");
-			break;
-		}
-			else if(numberG == 0) {
-			  System.out.println("choose a number between 0-10");
-		
+			} else if (numberG == 0) {
+				System.out.println("choose a number between 1-10");
+				numberG = number.nextInt();
+				guessingAttempts += 1;
 			}
-			else if(numberG < 7 ) {
-				System.out.println("the secret number is greater than your guess.  Guess again...");
-			}
-			else if(numberG > 7 ) {
-				System.out.println("the secret number is less than your guess.  Guess again");
-			}
-			
 			else {
 				System.out.println("You are wrong!  Please guess again");
-			}
 				
+			} if (randomNumber > numberG ) {
+				System.out.println("the secret number is greater than your guess.  Guess again...");
+				numberG = number.nextInt();
+			} else {
+				System.out.println("the secret number is less than your guess.  Guess again...");
+				numberG = number.nextInt();	
+			}
+				guessingAttempts += 1;
 		}
 		number.close();
 	}
 }
-	
-
